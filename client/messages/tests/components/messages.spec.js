676e22e7a78c550e73df399174d04d6659e50683
Tim Watson <twatson@ruffer.co.uk>
2016-11-02 12:43:04 +0000

    Added karma test runner

---
+++ client/messages/tests/components/messages.spec.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {expect} from 'chai';
+import Messages from '../../components/Messages';
+import { shallow } from 'enzyme';
+
+describe('<Messages/>', function () {
+
+
+});
---
+++ karma.conf.js
@@ -0,0 +1,122 @@
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['mocha'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'client/**/*.spec.js',
+      'server/**/*.spec.js'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      'client/**/*.js': ['webpack', 'sourcemap']
+    },
+
+    webpack: {
+      devtool: 'source-map',
+      output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+      },
+      plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new ExtractTextPlugin('style.css', {
+          allChunks: true
+        })
+      ],
+      module: {
+        loaders: [{
+          test: /\.js$/,
+          loaders: [ 'babel' ],
+          exclude: /node_modules/,
+          include: __dirname
+        }, {
+          test: /\.scss$/,
+          loader: ExtractTextPlugin.extract('css!sass')
+        }, {
+          test: /\.(woff|woff2|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/,
+          loader: 'url-loader?limit=1024&name=fonts/[name].[ext]'
+        },
+          {
+            test: /\.json$/,
+            loader: 'json',
+          }]
+      },
+      externals: {
+        'react/lib/ExecutionEnvironment': true,
+        'react/lib/ReactContext': true,
+        'react/addons': true
+      },
+      debug: true
+    },
+
+    webpackServer: {
+      noInfo: true //please don't spam the console when running in karma!
+    },
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9889,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['jsdom'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity,
+
+    plugins: [
+      'karma-mocha',
+      'karma-webpack',
+      'karma-sourcemap-loader',
+      'karma-jsdom-launcher'
+    ]
+  })
+}
